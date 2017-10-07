7191b291f651f72790c6b73a0e417851a18ce9c6
Unknown <marioriveravazquez@gmail.com>
2017-10-07 19:19:23 -0400

    V1.1

--- app/js/components/Global/Menu.jsx
+++ app/js/components/Global/Menu.jsx
@@ -108,9 +108,11 @@ export default class Menu extends Component {
 
   	render() {
     	return (
-      		<div className='Menu' id='Menu'>
-        		<Link to='/'><img onMouseOver={this.handleMouseOver2} onMouseOut={this.handleMouseOut2} src={this.state.homeSrc}></img></Link> <Link to='/About'><img onMouseOver={this.handleMouseOver3} onMouseOut={this.handleMouseOut3} src={this.state.aboutSrc}></img></Link> <img src={this.state.imgSrc} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} onClick={this.goHomePage}></img> <a href="http://nspnyc.bigcartel.com/"><img onMouseOver={this.handleMouseOver4} onMouseOut={this.handleMouseOut4} src={this.state.shopSrc}></img></a>  <Link to='/Cart'><img onMouseOver={this.handleMouseOver5} onMouseOut={this.handleMouseOut5} src={this.state.contactSrc}></img></Link><Link to='/Cart' id='cartLink'>CART</Link>
-        		<hr />
+      		<div className= 'topMenu'>
+      			<div className='Menu' id='Menu'>
+        			<Link to='/'><img onMouseOver={this.handleMouseOver2} onMouseOut={this.handleMouseOut2} src={this.state.homeSrc}></img></Link> <Link to='/About'><img onMouseOver={this.handleMouseOver3} onMouseOut={this.handleMouseOut3} src={this.state.aboutSrc}></img></Link> <img src={this.state.imgSrc} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} onClick={this.goHomePage}></img> <a href="http://nspnyc.bigcartel.com/"><img onMouseOver={this.handleMouseOver4} onMouseOut={this.handleMouseOut4} src={this.state.shopSrc}></img></a>  <Link to='/Cart'><img onMouseOver={this.handleMouseOver5} onMouseOut={this.handleMouseOut5} src={this.state.contactSrc}></img></Link>
+        			<hr />
+      			</div>
       		</div>
     	);
   	}
