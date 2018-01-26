215f4dc77c4eaa76715d0a4ff8cab2d3f064a46d
Unknown <marioriveravazquez@gmail.com>
2018-01-25 20:06:52 -0500

    Removed About

---
+++ app/js/views/Music.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class MusicNSP extends Component {
+	render() {
+		return(
+			<div className= "AboutNSP" id="AboutNSP">
+				AAAA
+			</div>
+		);
+	}
+}
+
