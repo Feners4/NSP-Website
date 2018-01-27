44688c54df44f54b6cdad08d42d898c5ac860b4f
Unknown <marioriveravazquez@gmail.com>
2018-01-27 12:49:18 -0500

    Updated Music link and Contact

--- app/js/views/Contact.js
+++ app/js/views/Contact.js
@@ -7,13 +7,16 @@ export default class Contact extends Component {
 		return(
 			<div className= "ContactInfo" id="ContactInfo">
 				<div id='ContactWrapper'>
-					EMAIL:
-					nocturnalsonsposse@gmail.com
+					<div id='emailWrapper'>
+						nocturnalsonsposse@gmail.com
+					</div>
 
-					INSTAGRAM:
-					@nocturnalsonsposse
-
-					<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/382477622&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+					<div id='instagramWrapper'>
+						INSTAGRAM:
+						<a href="http://www.instagram.com/nocturnalsonsposse"> 
+							@nocturnalsonsposse
+						</a>
+					</div>
 				</div>
 			</div>
 		);
--- app/js/views/Music.js
+++ app/js/views/Music.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class MusicNSP extends Component {
 	render() {
 		return(
-			<div className= "AboutNSP" id="AboutNSP">
-				AAAA
+			<div className= "MusicNSP" id="MusicNSP">
+				<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/382477622&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
 			</div>
 		);
 	}
